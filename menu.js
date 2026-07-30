bb38683ce07ed97af2015866d3ba26b608e5c695
SyazanaMOE <g-22424954@moe-dl.edu.my>
2026-07-30 08:15:32 +0800

    Add null checks for button event listeners

--- menu.js
+++ menu.js
@@ -1,35 +1,42 @@
 const ohmButton = document.getElementById("ohmButton");
 const resistanceButton = document.getElementById("resistanceButton");
 const arButton = document.getElementById("arButton");
-
-
 const pretestButton = document.getElementById("pretestButton");
 const posttestButton = document.getElementById("posttestButton");
 const aboutButton = document.getElementById("aboutButton");
 
+if (ohmButton) {
+    ohmButton.addEventListener("click", function () {
+        window.location.href = "ohm.html";
+    });
+}
 
-ohmButton.addEventListener("click", function () {
-    window.location.href = "ohm.html";
-});
+if (resistanceButton) {
+    resistanceButton.addEventListener("click", function () {
+        window.location.href = "resistance.html";
+    });
+}
 
-resistanceButton.addEventListener("click", function () {
-    window.location.href = "resistance.html";
-});
+if (arButton) {
+    arButton.addEventListener("click", function () {
+        window.location.href = "ar.html";
+    });
+}
 
-arButton.addEventListener("click", function () {
-    window.location.href = "ar.html";
-});
+if (pretestButton) {
+    pretestButton.addEventListener("click", function () {
+        window.location.href = "pretest.html";
+    });
+}
 
+if (posttestButton) {
+    posttestButton.addEventListener("click", function () {
+        window.location.href = "posttest.html";
+    });
+}
 
-
-pretestButton.addEventListener("click", function () {
-    window.location.href = "pretest.html";
-});
-
-posttestButton.addEventListener("click", function () {
-    window.location.href = "posttest.html";
-});
-
-aboutButton.addEventListener("click", function () {
-    window.location.href = "about.html";
-});
+if (aboutButton) {
+    aboutButton.addEventListener("click", function () {
+        window.location.href = "about.html";
+    });
+}
